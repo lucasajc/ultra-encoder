55377600bf4eb26cd39845137f821b51bf83cffd
Lucas de Aguilar <lucasdeaguilarjc@hotmail.com>
2018-11-01 16:57:31 -0300

    Update Constants.java

--- src/main/java/com/app/ultraencoder/utils/Constants.java
+++ src/main/java/com/app/ultraencoder/utils/Constants.java
@@ -6,9 +6,9 @@ package com.app.ultraencoder.utils;
 public class Constants{
 	public static String awsS3ClientRegion = "sa-east-1";
 	public static String awsS3BucketName = "ultra-encoder";
-	public static String awsS3Key = "AKIAIFWRUUN3GBEWPICQ";
-	public static String awsS3KeySecret = "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";
-	public static String zencoderKey = "1b96ed3c67d90cee88560e9a7df260dc";
+	public static String awsS3Key = "";
+	public static String awsS3KeySecret = "";
+	public static String zencoderKey = "";
 	public static String mp4Format = "mp4";
 	public static String zencoderFinishedStatus = "finished";
 	public static int expireMinutes = 60;
